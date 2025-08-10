cd55747370b4e05e5e1ad95836dab0d782f6f6c6
tcdw <tcdw2011@gmail.com>
2025-08-10 21:24:21 +0800

    chore: ignore git directory

--- sync.sh
+++ sync.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 pnpm run build
-rsync -av --progress --delete-after --exclude node_modules . tcdw@tcdw.host.reall.bond:/home/tcdw/apps/SilverBlog/templates/koi
+rsync -av --progress --delete-after --exclude node_modules --exclude .git . tcdw@tcdw.host.reall.bond:/home/tcdw/apps/SilverBlog/templates/koi
 curl -X DELETE https://www.tcdw.net/cache/
